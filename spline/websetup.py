b69e6547ad10ae0316ef69b598b6c079d18843a6
Eevee <eevee@nyarumaa.(none)>
2008-11-16 20:12:56 -0500

    Initial Spline commit; a ase Pylons 0.9.7 beta project.

---
+++ spline/websetup.py
@@ -0,0 +1,17 @@
+"""Setup the spline application"""
+import logging
+
+from spline.config.environment import load_environment
+
+log = logging.getLogger(__name__)
+
+def setup_app(command, conf, vars):
+    """Place any commands to setup spline here"""
+    load_environment(conf.global_conf, conf.local_conf)
+
+    # Load the models
+    from spline.model import meta
+    meta.metadata.bind = meta.engine
+
+    # Create the tables if they aren't there already
+    meta.metadata.create_all(checkfirst=True)
