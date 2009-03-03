cbf3544740d664c5ac28cf09c80f4343807501c0
Eevee <git@veekun.com>
2009-03-03 00:26:19 -0800

    Minor tidying of setup.py.

--- setup.py
+++ setup.py
@@ -19,14 +19,17 @@ setup(
     ],
     setup_requires=["PasteScript==dev,>=1.6.3dev-r7326"],
     packages=find_packages(exclude=['ez_setup']),
+
     include_package_data=True,
-    test_suite='nose.collector',
     package_data={'spline': ['i18n/*/LC_MESSAGES/*.mo']},
+    zip_safe=False,
+
+    test_suite='nose.collector',
+
     #message_extractors = {'spline': [
     #        ('**.py', 'python', None),
     #        ('templates/**.mako', 'mako', None),
     #        ('public/**', 'ignore', None)]},
-    zip_safe=False,
     paster_plugins=['PasteScript', 'Pylons'],
     entry_points="""
     [paste.app_factory]
