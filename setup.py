a3b2af81a4c22b69b9ea06859fb4df389cc9e87a
Eevee <git@veekun.com>
2010-09-06 20:34:26 -0700

    Added markdown support to core.  Forums will use it, but other stuff may too.  #262

--- setup.py
+++ setup.py
@@ -18,6 +18,8 @@ setup(
         "Mako",
         "nose>=0.11",
         "WTForms>=0.6",
+        'markdown',
+        'lxml',
     ],
     setup_requires=["PasteScript==dev,>=1.6.3dev-r7326"],
     packages=find_packages(exclude=['ez_setup']),
---
+++ spline/lib/markdown.py
@@ -0,0 +1,70 @@
+"""Handles Markdown translation."""
+from __future__ import absolute_import
+
+import lxml.html
+import lxml.html.clean
+import markdown
+
+markdown_extensions = []
+
+def register_extension(extension):
+    """Registers the given markdown extension.
+
+    This is global and permanent; use with care!
+    """
+    if not extension in markdown_extensions:
+        markdown_extensions.append(extension)
+
+def translate(raw_text):
+    """Takes a unicode string of Markdown source.  Returns HTML."""
+
+    # First translate the markdown
+    md = markdown.Markdown(
+        extensions=markdown_extensions,
+        output_format='xhtml1',
+    )
+
+    html = md.convert(raw_text)
+
+    # Then sanitize the HTML -- whitelisting only, thanks!
+    # Make this as conservative as possible to start.  Might loosen it up a bit
+    # later.
+    fragment = lxml.html.fromstring(html)
+
+    cleaner = lxml.html.clean.Cleaner(
+        scripts = True,
+        javascript = True,
+        comments = True,
+        style = True,
+        links = True,
+        meta = True,
+        page_structure = True,
+        #processing_instuctions = True,
+        embedded = True,
+        frames = True,
+        forms = True,
+        annoying_tags = True,
+        safe_attrs_only = True,
+
+        remove_unknown_tags = False,
+        allow_tags = [
+            # Structure
+            'p', 'div', 'span', 'ul', 'ol', 'li',
+
+            # Tables
+            'table', 'thead', 'tbody', 'tfoot', 'tr', 'th', 'td',
+
+            # Embedding
+            'a',
+
+            # Oldschool styling
+            'strong', 'b', 'em', 'i', 's', 'u',
+        ],
+    )
+    cleaner(fragment)
+
+    # Autolink URLs
+    lxml.html.clean.autolink(fragment)
+
+    # And, done.  Flatten the thing and return it
+    return lxml.html.tostring(fragment)
